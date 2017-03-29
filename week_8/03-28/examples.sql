eb641a301ba87d027cdea1c1e573abf7220b6a40
Jonathan Shaw <jonathan.y.shaw@gmail.com>
2017-03-29 09:23:20 -0400

    added exercises 3-28

---
+++ week_8/03-28/examples.sql
@@ -0,0 +1,63 @@
+
+--Select technolgy used by personal website
+select
+	project.name, tech.name
+from
+	project_uses_tech
+inner join
+	project on project.id = project_uses_tech.project_id
+inner join
+	tech on tech.id = project_uses_tech.tech_id
+where project.name = 'Personal Website';
+
+--perform a left outer join from tech table to the proejct_uses_tech table, which tech has no associated project?
+select
+	project.name, tech.name
+from
+	tech
+
+left outer join
+	project_uses_tech on tech.id = project_uses_tech.tech_id
+
+left outer join
+	project on project.id = project_uses_tech.project_id
+
+--count technology used on each project
+select
+	project.name, count(tech.name)
+from
+	project
+
+left outer join
+	project_uses_tech on project.id = project_uses_tech.project_id
+
+left outer join
+	tech on tech.id = project_uses_tech.project_id
+
+group by project.name;
+
+--join tech and project, which project has no associated tech
+select
+	project.name, tech.name
+from
+	project
+
+left outer join
+	project_uses_tech on project.id = project_uses_tech.project_id
+
+left outer join
+	tech on tech.id = project_uses_tech.project_id
+
+-- count number of times each project uses each tech
+select
+	count(project.name), tech.name
+from
+	tech
+
+left outer join
+	project_uses_tech on tech.id = project_uses_tech.tech_id
+
+left outer join
+	project on project.id = project_uses_tech.project_id
+
+group by tech.name
